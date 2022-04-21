92994dc794794935bcb1f610785e0610bdd713a8
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-21 13:05:15 +0300

    Add problem solution

---
+++ TripleOfLatinLetters.js
@@ -0,0 +1,13 @@
+function tripleOfLatinLetters(number) {
+    let num = Number(number);
+
+    for (let i = 97; i < 97 + num; i++) {
+        for (let j = 97; j < 97 + num; j++) {
+            for (let k = 97; k < 97 + num; k++) {
+                console.log(`${String.fromCharCode(i)}${String.fromCharCode(j)}${String.fromCharCode(k)}`);
+            }
+        }
+    }
+
+}
+tripleOfLatinLetters('2');
