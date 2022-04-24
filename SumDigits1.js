437b0b068e324f9551843eae57935278717a1a04
StanislavHrusanov <stanislavhrusanov@gmail.com>
2022-04-24 09:34:19 +0300

    Add problem solution

---
+++ SumDigits1.js
@@ -0,0 +1,9 @@
+function sumDigits1(number) {
+    let sum = 0
+    while (number > 0) {
+        sum += number % 10;
+        number = Math.floor(number / 10);
+    }
+    console.log(sum);
+}
+sumDigits1(543)
